ae6247ed937b11e8e67f48e1289d2ce1381c6e51
Edir Lucas Icety <icetylucas@gmail.com>
2023-06-02 12:16:38 -0300

    primeiro desafio

--- dev-eficiente-heuristicas/src/main/java/com/deveficiente/heuristicas/coesaobasica/treinamentoseatividades/v1/Treinamento.java
+++ dev-eficiente-heuristicas/src/main/java/com/deveficiente/heuristicas/coesaobasica/treinamentoseatividades/v1/Treinamento.java
@@ -18,13 +18,32 @@ public class Treinamento {
 		
 		/*
 		 * Primeiro passo é vocé implementar a adição de titulos de atividades
-		 * dentro do treinamento. Como você faria? 
+		 * dentro do treinamento. Como você faria?
 		 */
+
+		testeDeFogo.addTituloAtividade("Atividade 1");
+		testeDeFogo.addTituloAtividade("Atividade 2");
+		testeDeFogo.addTituloAtividade("Atividade 3");
 
 		/*
 		 * Uma vez que você implementou a adição de titulos de atividades,
 		 * é necessário que você possibilite a descoberta da posicao do titulo
 		 * da atividade dentro do treinamento. 
 		 */
+
+		int posicaoAtividade1 = testeDeFogo.posicaoTituloAtividade("Atividade 1");
+		int posicaoAtividade2 = testeDeFogo.posicaoTituloAtividade("Atividade 2");
+		int posicaoAtividade3 = testeDeFogo.posicaoTituloAtividade("Atividade 3");
+		System.out.println("A posição da Atividadade 1 é " + posicaoAtividade1);
+		System.out.println("A posição da Atividadade 2 é " + posicaoAtividade2);
+		System.out.println("A posição da Atividadade 3 é " + posicaoAtividade3);
+	}
+
+	private int posicaoTituloAtividade(String tituloAtividade) {
+		return this.titulosAtividades.indexOf(tituloAtividade) + 1;
+	}
+
+	private void addTituloAtividade(String titulo) {
+		this.titulosAtividades.add(titulo);
 	}
 }
